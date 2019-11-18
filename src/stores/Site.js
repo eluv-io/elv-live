d92ab0cbbb7c32e181b2e9b7ee5aca6223619fac
Kevin Talmadge <kevin@eluv.io>
2019-11-18 14:20:37 -0800

    Update to new asset format ; Produce playout options and images for trailers and clips ; Switch to playing trailer by default

--- src/stores/Site.js
+++ src/stores/Site.js
@@ -11,6 +11,23 @@ class SiteStore {
     this.rootStore = rootStore;
   }
 
+  async ResolveImageLinks({versionHash, imageSpec}) {
+    let images = {};
+
+    if(imageSpec) {
+      await Promise.all(
+        Object.keys(imageSpec).map(async image => {
+          images[image] = await this.rootStore.client.LinkUrl({
+            versionHash,
+            linkPath: `asset_metadata/images/${image}`
+          });
+        })
+      );
+    }
+
+    return images;
+  }
+
   @action.bound
   LoadSite = flow(function * (objectId) {
     this.site = undefined;
@@ -78,23 +95,74 @@ class SiteStore {
 
     yield Promise.all(
       Object.keys(franchise.titles).map(async titleKey => {
+        const titleInfo = this.franchises[franchiseKey].titles[titleKey];
+
         const titleVersionHash = await client.LinkTarget({
           versionHash: franchise.versionHash,
           linkPath: `asset_metadata/titles/${titleKey}`
         });
 
-        const publicInfo = await client.ContentObjectMetadata({
-          versionHash: titleVersionHash,
-          metadataSubtree: "public"
-        });
+        // Resolve images and playout options for trailers
+        let trailers = {};
+        if(titleInfo.trailers) {
+          await Promise.all(
+            Object.keys(titleInfo.trailers).map(async key => {
+              const trailerHash = await client.LinkTarget({
+                versionHash: titleVersionHash,
+                linkPath: `asset_metadata/trailers/${key}`
+              });
+
+              trailers[key] = {
+                ...titleInfo.trailers[key],
+                images: await this.ResolveImageLinks({
+                  versionHash: trailerHash,
+                  imageSpec: titleInfo.trailers[key].images
+                }),
+                playoutOptions: await client.BitmovinPlayoutOptions({
+                  versionHash: trailerHash,
+                  protocols: ["dash", "hls"]
+                })
+              };
+            })
+          );
+        }
+
+        // Resolve images and playout options for clips
+        let clips = {};
+        if(titleInfo.clips) {
+          await Promise.all(
+            Object.keys(titleInfo.clips).map(async key => {
+              const clipHash = await client.LinkTarget({
+                versionHash: titleVersionHash,
+                linkPath: `asset_metadata/clips/${key}`
+              });
+
+              clips[key] = {
+                ...titleInfo.clips[key],
+                images: await this.ResolveImageLinks({
+                  versionHash: clipHash,
+                  imageSpec: titleInfo.clips[key].images
+                }),
+                playoutOptions: await client.BitmovinPlayoutOptions({
+                  versionHash: clipHash,
+                  protocols: ["dash", "hls"]
+                })
+              };
+            })
+          );
+        }
 
         /*
-        // hls.js / dash.js
-        const playoutOptions = await client.PlayoutOptions({
-          versionHash: titleVersionHash,
-          protocols: ["hls"]
-        });
+          // hls.js / dash.js
+          const playoutOptions = await client.PlayoutOptions({
+            versionHash: titleVersionHash,
+            protocols: ["hls"]
+          });
         */
+        const images = await this.ResolveImageLinks({
+          versionHash: titleVersionHash,
+          imageSpec: titleInfo.images
+        });
 
         const playoutOptions = await client.BitmovinPlayoutOptions({
           versionHash: titleVersionHash,
@@ -105,33 +173,15 @@ class SiteStore {
           versionHash: titleVersionHash
         });
 
-        const titleInfo = this.franchises[franchiseKey].titles[titleKey];
-
-        let components = {};
-        await Promise.all(
-          Object.keys(titleInfo.components).map(async component => {
-            try {
-              components[component] = await client.LinkUrl({
-                versionHash: titleVersionHash,
-                linkPath: `asset_metadata/components/${component}`
-              });
-            } catch (error){
-              // eslint-disable-next-line no-console
-              console.error(`Unable to load component at 'asset_metadata/components/${component}':`);
-              // eslint-disable-next-line no-console
-              console.error(error);
-            }
-          })
-        );
-
         this.titles[titleKey] = {
           ...titleInfo,
-          components,
+          images,
+          clips,
+          trailers,
           playoutOptions,
           key: titleKey,
           versionHash: titleVersionHash,
-          name: publicInfo.name || "",
-          description: publicInfo.description || ""
+          name: titleInfo.title || titleInfo.name || ""
         };
       })
     );
