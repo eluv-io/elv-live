2fc8d2ac4532ec2b46875556670bf6c0aefc98fb
Kevin Talmadge <kevin@eluv.io>
2020-02-05 16:22:33 -0800

    Switch to master branch of elv-components-js ; Minor styling changes

--- dist/configuration.js
+++ dist/configuration.js
@@ -1,7 +1,7 @@
 const EluvioConfiguration = {
   "config-url": "https://demov3.net955210.contentfabric.io/config",
   "site-id": "iq__3ZUhwxucJXDjhPoPAiW2jjPzyJaP",
-  "config-url": "https://main.net955210.contentfabric.io/config",
-  "site-id": "iq__3hgxWJ8uNVYWyT7QbEafrqfLMJTv",
+  //"config-url": "https://main.net955210.contentfabric.io/config",
+  //"site-id": "iq__3hgxWJ8uNVYWyT7QbEafrqfLMJTv",
   "version": "local"
 };
