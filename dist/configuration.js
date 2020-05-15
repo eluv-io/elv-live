5eec722ca82a10f599abc0c2bffe7ccbd76ab1b0
Kevin Talmadge <kevin@eluv.io>
2020-05-15 11:29:36 -0700

    Remove build from git

--- dist/configuration.js
+++
@@ -1,7 +0,0 @@
-const EluvioConfiguration = {
-  "config-url": "https://main.net955305.contentfabric.io/config",
-  //"site-id": "iq__3ZUhwxucJXDjhPoPAiW2jjPzyJaP",
-  //"config-url": "https://main.net955210.contentfabric.io/config",
-  //"site-id": "iq__3hgxWJ8uNVYWyT7QbEafrqfLMJTv",
-  "version": "local"
-};
