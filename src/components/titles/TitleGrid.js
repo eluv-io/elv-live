f39794541bd7b9b7c2e7ad01c58df5e3caafef8e
Kevin Talmadge <kevin@eluv.io>
2020-05-16 14:43:56 -0700

    Update to support series, seasons, episodes

--- src/components/titles/TitleGrid.js
+++ src/components/titles/TitleGrid.js
@@ -3,107 +3,49 @@ import PropTypes from "prop-types";
 import {inject, observer} from "mobx-react";
 import TitleIcon from "./TitleIcon";
 
-import ClearSearchIcon from "../../static/icons/clear.svg";
-import {IconButton, LoadingElement} from "elv-components-js";
-
 @inject("siteStore")
 @observer
 class TitleGrid extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      search: "",
-      searching: false
-    };
-
-    this.HandleSearchChange = this.HandleSearchChange.bind(this);
-  }
-
-  // Debounce filter input
-  HandleSearchChange(event) {
-    const value = event.target.value;
-
-    if(this.state.searchTimeout) {
-      clearTimeout(this.state.searchTimeout);
-    }
-
-    this.setState({
-      search: value,
-      searching: true,
-      searchTimeout: setTimeout(
-        async () => {
-          const initialSearch = this.state.search;
-          try {
-            await this.props.siteStore.SearchTitles({query: this.state.search});
-          } finally {
-            // Don't clear indicator if search query has changed
-            if(this.state.search === initialSearch) {
-              this.setState({searching: false});
-            }
-          }
-        }, 1000
-      )
-    });
-  }
-
-  Search() {
-    return (
-      <div className="title-search">
-        <input value={this.state.search} onChange={this.HandleSearchChange} placeholder="Filter titles..."/>
-        <IconButton icon={ClearSearchIcon} className="clear-search" title="Clear" onClick={() => this.HandleSearchChange({target: { value: "" }})} />
-      </div>
-    );
-  }
-
   render() {
-    const playlist = this.props.playlistIndex !== undefined && this.props.siteStore.playlists[this.props.playlistIndex];
-    const reelTitle = playlist && playlist.name || this.props.channels && "Channels" || "All Titles";
-    const hasSearch = reelTitle === "All Titles" && this.props.siteStore.siteInfo.indexHash;
+    if(!this.props.titles || this.props.titles.length === 0) { return null; }
 
-    let titles;
-    if(playlist) {
-      titles = playlist.titles;
-    } else if(this.props.channels) {
-      titles = this.props.siteStore.channels;
-    } else {
-      titles = this.props.siteStore.titles;
+    let titles = this.props.titles;
+    if(this.props.siteStore.searchQuery) {
+      titles = titles.filter(title => this.props.siteStore.filteredTitles.includes(title.objectId));
     }
 
-    if(titles.length === 0) {
-      return null;
-    }
+    if(titles.length === 0) { return null; }
 
     return (
       <div className="title-grid-container">
-        <h3 className="title-grid-header">{ reelTitle } {hasSearch ? this.Search() : null}</h3>
-        <LoadingElement loading={this.state.searching} loadingClassname="loading-indicator">
-          <div className="title-grid-titles">
-            {
-              titles.map((title, index) => {
-                if(hasSearch && this.state.search && !this.props.siteStore.filteredTitles.includes(title.objectId)) {
-                  return null;
-                }
+        <h3 className="title-grid-header">{ this.props.name }</h3>
+        <div className="title-grid-titles">
+          {
+            titles.map((title, index) => {
+              if(this.props.siteStore.searchQuery && !this.props.siteStore.filteredTitles.includes(title.objectId)) {
+                return null;
+              }
 
-                return (
-                  <TitleIcon
-                    key={`title-grid-title-${index}-${this.props.playlistIndex}`}
-                    title={title}
-                    visible
-                  />
-                );
-              })
-            }
-          </div>
-        </LoadingElement>
+              return (
+                <TitleIcon
+                  key={`title-grid-title-${this.props.name}-${index}`}
+                  title={title}
+                  visible
+                />
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
 }
 
 TitleGrid.propTypes = {
-  channels: PropTypes.bool,
-  playlistIndex: PropTypes.number
+  name: PropTypes.string.isRequired,
+  titles: PropTypes.arrayOf(
+    PropTypes.object
+  ).isRequired
 };
 
 export default TitleGrid;
--- src/components/titles/TitleReel.js
+++ src/components/titles/TitleReel.js
@@ -16,28 +16,21 @@ class TitleReel extends React.Component {
   }
 
   render() {
-    const playlist = this.props.playlistIndex !== undefined && this.props.siteStore.playlists[this.props.playlistIndex];
-    const reelTitle = playlist && playlist.name || this.props.channels && "Channels" || "All Titles";
+    if(!this.props.titles || this.props.titles.length === 0) { return null; }
 
-    let titles;
-    if(playlist) {
-      titles = playlist.titles;
-    } else if(this.props.channels) {
-      titles = this.props.siteStore.channels;
-    } else {
-      titles = this.props.siteStore.titles;
+    let titles = this.props.titles;
+    if(this.props.siteStore.searchQuery) {
+      titles = titles.filter(title => this.props.siteStore.filteredTitles.includes(title.objectId));
     }
 
-    if(titles.length === 0) {
-      return null;
-    }
+    if(titles.length === 0) { return null; }
 
     const showLeft = this.state.startIndex !== 0;
-    const showRight = this.state.startIndex + this.state.visible < titles.length;
+    const showRight = this.state.startIndex + this.state.visible < this.props.titles.length;
 
     return (
       <div className="title-reel-container">
-        <h3 className="title-reel-header">{ reelTitle }</h3>
+        <h3 className="title-reel-header">{ this.props.name }</h3>
         <div className="title-reel">
           <div
             className={`reel-arrow reel-arrow-left ${showLeft ? "" : "hidden"}`}
@@ -53,7 +46,7 @@ class TitleReel extends React.Component {
             {
               titles.map((title, index) => (
                 <TitleIcon
-                  key={`title-reel-title-${index}-${this.props.playlistIndex}`}
+                  key={`title-reel-title-${this.props.name}-${index}`}
                   title={title}
                   visible={index >= this.state.startIndex && index < this.state.startIndex + this.state.visible}
                 />
@@ -77,8 +70,10 @@ class TitleReel extends React.Component {
 }
 
 TitleReel.propTypes = {
-  channels: PropTypes.bool,
-  playlistIndex: PropTypes.number
+  name: PropTypes.string.isRequired,
+  titles: PropTypes.arrayOf(
+    PropTypes.object
+  ).isRequired
 };
 
 export default TitleReel;
