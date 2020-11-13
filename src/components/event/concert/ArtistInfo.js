924039975f4ed8c2f1c84be6528b2975271ba09b
Alec Jo <alecjo@Alecs-MBP.attlocal.net>
2020-11-12 20:29:06 -0800

    event demo

--- src/components/event/concert/ArtistInfo.js
+++ src/components/event/concert/ArtistInfo.js
@@ -2,6 +2,8 @@ import React from "react";
 import {inject, observer} from "mobx-react";
 import {ImageIcon} from "elv-components-js";
 import {Link} from "react-router-dom";
+import mad1 from "../../../static/images/1MAD.jpg";
+import mad2 from "../../../static/images/2MAD.jpg";
 
 @inject("rootStore")
 @inject("siteStore")
@@ -24,19 +26,34 @@ class ArtistInfo extends React.Component {
           className="premiereTabs__container__poster" 
           title="Poster" 
         /> */}
+        <div className="event-container__info__ticketGroup">
 
         <div className="event-container__info">
           <div className="event-container__info__h1Props">
             <div>{ eventInfo.name }</div>
           </div>
 
-          <div className="event-container__info__synopsis">
-          Prior to signing with epic records late last year, madison beer achieved unprecedented success as an independent artist with her partner first access entertainment. Her debut ep as she pleases boasts over 950 million streams and made her the first independent female solo artist to break into the top 20 us pop radio charts, after having debuted in the top 5 on itunes in 18 countries and in the top 10 in 42 countries worldwide. Globally, beer has over 2 billion streams across her catalogue.
-    </div>
-          <div className="event-container__info__synopsis">
-          She’s attracted the endorsement of time, rolling stone, nme, v magazine and billboard who named her among its coveted “21 under 21” list, and has a social following that reaches an audience of over 20 million on instagram and 3 million on twitter. .
-</div>
+            <div className="event-container__info__synopsis">
+            Prior to signing with epic records late last year, madison beer achieved unprecedented success as an independent artist with her partner first access entertainment. Her debut ep as she pleases boasts over 950 million streams and made her the first independent female solo artist to break into the top 20 us pop radio charts, after having debuted in the top 5 on itunes in 18 countries and in the top 10 in 42 countries worldwide. Globally, beer has over 2 billion streams across her catalogue.
+                </div>
+            <div className="event-container__info__synopsis">
+            She’s attracted the endorsement of time, rolling stone, nme, v magazine and billboard who named her among its coveted “21 under 21” list, and has a social following that reaches an audience of over 20 million on instagram and 3 million on twitter. .
+            </div>
+          </div>
           <iframe className="event-container__info__music" src="https://open.spotify.com/embed/album/5boeEaUtj7gHXFxKtFFlzL" width="400" height="350" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    
+
+      </div>
+      <div className="event-container__info__ticketGroup">
+        <img
+              src={mad1}
+              className= "event-container__info__photo2"
+            />     
+               <img
+              src={mad2}
+              className= "event-container__info__photo2"
+            />     
+
 
         </div>
       </div>
--- src/components/event/concert/ConcertOverview.js
+++ src/components/event/concert/ConcertOverview.js
@@ -2,6 +2,7 @@ import React from "react";
 import {inject, observer} from "mobx-react";
 import {ImageIcon} from "elv-components-js";
 import {Link} from "react-router-dom";
+import poster from "../../../static/images/livePOSTER.jpg";
 
 @inject("rootStore")
 @inject("siteStore")
@@ -24,19 +25,26 @@ class ConcertOverview extends React.Component {
           className="premiereTabs__container__poster" 
           title="Poster" 
         /> */}
+        <div className="event-container__info__ticketGroup">
 
-        <div className="event-container__info">
-          <div className="event-container__info__h1Props">
-            <div>{ eventInfo.name }</div>
-            <div className="event-container__info__h1Props__desc">{`"Life Support World Tour" Live Virtual Concert`}</div>
+          <div className="event-container__info">
+            <div className="event-container__info__h1Props">
+              <div>{ eventInfo.name }</div>
+              <div className="event-container__info__h1Props__desc">{`"As She Pleases World Tour" Live Virtual Concert`}</div>
+            </div>
+
+            <div className="event-container__info__synopsis">
+            { eventInfo.name } will be making history on December 17th with a global live stream from the legendary Los Angeles venue, the Staple Center, to celebrate the release of her debut solo album: Life Support. Tickets and VIP packages for this historic streaming event are on sale now.     </div>
+            <div className="event-container__info__synopsis">
+            The stream will feature a full arena production complete with a visual feast of lights and pyrotechnics, a stacked set list featuring all tracks from Life Support, her top hits, a handful of covers, and a guest performance from Halsey. A special pre-show will kick-off 1 hour before the event featuring exclusive interviews and behind-the-scenes footage.</div>
+            {/* <iframe src="https://open.spotify.com/embed/album/5boeEaUtj7gHXFxKtFFlzL" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
           </div>
-
-          <div className="event-container__info__synopsis">
-          { eventInfo.name } will be making history on December 17th with a global live stream from the legendary Los Angeles venue, the Staple Center, to celebrate the release of her debut solo album: Life Support. Tickets and VIP packages for this historic streaming event are on sale now.     </div>
-          <div className="event-container__info__synopsis">
-          The stream will feature a full arena production complete with a visual feast of lights and pyrotechnics, a stacked set list featuring all tracks from Life Support, her top hits, a handful of covers, and a guest performance from Halsey. A special pre-show will kick-off 1 hour before the event featuring exclusive interviews and behind-the-scenes footage.</div>
-          {/* <iframe src="https://open.spotify.com/embed/album/5boeEaUtj7gHXFxKtFFlzL" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
-          <div className="event-container__info__ticketGroup">
+          <img
+              src={poster}
+              className= "event-container__info__photo"
+            />         
+        </div>
+        <div className="event-container__info__ticketGroup">
             <div className="event-container__info__ticketDetail">
               <div className="event-container__info__ticketDetail2">
                 General Admission
@@ -57,30 +65,28 @@ class ConcertOverview extends React.Component {
 
             </div>
 
-          <div className="event-container__info__ticketDetail">
-            <div className="event-container__info__ticketDetail2">
-              VIP Package
+            <div className="event-container__info__ticketDetail">
+              <div className="event-container__info__ticketDetail2">
+                VIP Package
+              </div>
+              <ul className="event-container__info__ticketDetail3">
+                <li className="event-container__info__ticketDetail4">- One Virtual Ticket to Live Stream Concert</li>
+                <li className="event-container__info__ticketDetail4">- Special Access to Live Chat and Virtual Fan Wall</li>
+                <li className="event-container__info__ticketDetail4">- Exclusive Virtual Meet and Greet with { eventInfo.name }</li>
+
+              </ul>     
+              <Link to={{
+                  pathname: `/payment/${this.props.name}`,
+                  state: {
+                    name: eventInfo.name,
+                    icon: eventInfo.icon
+                  }
+                }}>
+                  <button type="button" className="btn2 btn2--white btn3 btn3--white event-container__info__ticketBTN">$40.00</button>
+                </Link> 
             </div>
-            <ul className="event-container__info__ticketDetail3">
-              <li className="event-container__info__ticketDetail4">- One Virtual Ticket to Live Stream Concert</li>
-              <li className="event-container__info__ticketDetail4">- Special Access to Live Chat and Virtual Fan Wall</li>
-              <li className="event-container__info__ticketDetail4">- Exclusive Virtual Meet and Greet with { eventInfo.name }</li>
-
-            </ul>     
-            <Link to={{
-                pathname: `/payment/${this.props.name}`,
-                state: {
-                  name: eventInfo.name,
-                  icon: eventInfo.icon
-                }
-              }}>
-                <button type="button" className="btn2 btn2--white btn3 btn3--white event-container__info__ticketBTN">$40.00</button>
-              </Link> 
-          </div>
 
           </div>
-          
-        </div>
       </div>
     );
   }
