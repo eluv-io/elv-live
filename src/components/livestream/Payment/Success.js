146bbfa10e690f8dbe999aa4ee9cbbd3087630e8
Alec Jo <alecjo@Alecs-MBP.attlocal.net>
2020-11-25 22:09:32 -0800

    revert button

--- src/components/livestream/Payment/Success.js
+++ src/components/livestream/Payment/Success.js
@@ -39,7 +39,7 @@ class Success extends React.Component {
                 {/* <h1 className="code-reveal__ticket__ticketdetail">Ticket Code:</h1>
                 <h2 className="code-reveal__ticket__ticketdetail2">{this.props.rootStore.OTPCode} </h2> */}
                 <div className="sr-section completed-view">
-                  <Link to="/code" className="btn2 btn2--darkblue buttonguy">Redeem Code</Link>
+                  <Link to="/rita-ora/d457a576" className="btn2 btn2--darkblue buttonguy">Back to Event</Link>
                 </div>
               </div>
             </div>
