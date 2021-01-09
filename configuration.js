06aed3769f260f54626aa8ba93828b349c7712a4
Alec Jo <alecjo@Alecs-MBP.attlocal.net>
2021-01-08 17:04:01 -0800

    deploy test site

--- configuration.js
+++ configuration.js
@@ -8,17 +8,14 @@ const EluvioConfiguration = {
   // Demo
   "config-url": "https://demov3.net955210.contentfabric.io/config",
 
-  // Eluvio Live Platform: ilib2bLJtRJG3LW9yScSNStbjkSqF2hH  iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv
-  // Eluvio Live Platform - DEV: ilib2bLJtRJG3LW9yScSNStbjkSqF2hH  iq__23edex4Fwgt98PzR7mMrMHp8dw6E
+  // PROD Site (https://live.eluv.io/) for main branch using Eluvio Live Platform: ilib2bLJtRJG3LW9yScSNStbjkSqF2hH  iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv
   // "library-id": "ilib2bLJtRJG3LW9yScSNStbjkSqF2hH",
-  // "object-id": "iq__23edex4Fwgt98PzR7mMrMHp8dw6E",
+  // "object-id": "iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv"
+
+
+  // TEST/STAGING Site (https://live-stg-eluv-io.web.app/) for develop branch using Eluvio Live Platform - DEV: ilib2bLJtRJG3LW9yScSNStbjkSqF2hH  iq__23edex4Fwgt98PzR7mMrMHp8dw6E
   "library-id": "ilib2bLJtRJG3LW9yScSNStbjkSqF2hH",
-  "object-id": "iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv",
-  //Prod Site: https://live.eluv.io/
-  //Test Site: https://live-stg-eluv-io.web.app/,
-  //Prod Site: https://live.eluv.io/,
-  "deploy-link": "https://live.eluv.io/",
-  // "deploy-link": "https://live-stg-eluv-io.web.app/",
+  "object-id": "iq__23edex4Fwgt98PzR7mMrMHp8dw6E",
   "version": "local"
 };
 
