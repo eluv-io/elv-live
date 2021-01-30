1c0d76a05e944f48eac9d2f4f9760526e93be9f9
Kevin Talmadge <kevin@eluv.io>
2021-01-29 16:51:17 -0800

    Change config back to demo network

--- src/EluvioConfiguration.js
+++ src/EluvioConfiguration.js
@@ -5,14 +5,14 @@ const EluvioConfiguration = {
   //"object-id": "iq__23edex4Fwgt98PzR7mMrMHp8dw6E",
 
   // Eluvio Live Site Object - PROD (https://live.eluv.io/) ro3 object: ilib2bLJtRJG3LW9yScSNStbjkSqF2hH  iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv
-  //"config-url": "https://demov3.net955210.contentfabric.io/config",
-  //"object-id": "iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv",
+  "config-url": "https://demov3.net955210.contentfabric.io/config",
+  "object-id": "iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv",
 
 
 
   // Test network
-  "config-url": "https://test.net955203.contentfabric.io/config",
-  "object-id": "iq__zK6CLwYBLhZmosiMXMe9eg2zErE",
+  //"config-url": "https://test.net955203.contentfabric.io/config",
+  //"object-id": "iq__zK6CLwYBLhZmosiMXMe9eg2zErE",
 
   "tenantId": "iten3tNEk7iSesexWeD1mGEZLwqHGMjB",
   "ga-ntpId": "QOTPZsAzK5pU7xe",
