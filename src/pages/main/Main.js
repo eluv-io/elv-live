e41d6843ede6d62129cf974591bab009280001b0
Kevin Talmadge <kevin@eluv.io>
2021-03-04 14:45:28 -0800

    Main page feature block

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -4,6 +4,9 @@ import {inject, observer} from "mobx-react";
 import Copy from "./copy/Copy.yaml";
 import FeaturedEvents from "Pages/main/components/FeaturedEvents";
 import Logo from "Assets/images/logo/coloredEluvioLiveLogo.png";
+import FeatureBlock from "Pages/main/components/FeatureBlock";
+
+import TEMPIMAGE from "Assets/images/ritaora/heroRita.jpg";
 
 @inject("siteStore")
 @observer
@@ -17,6 +20,28 @@ class Main extends React.Component {
         </div>
         <div className="main-page__content-container">
           <h2 className="main-page__header">{ Copy.main.header }</h2>
+          <div className="main-page__features-container">
+            <FeatureBlock
+              copyKey="beautiful_quality"
+              image={<img src={TEMPIMAGE} />}
+            />
+            <FeatureBlock
+              copyKey="directly_to_fans"
+              image={<img src={TEMPIMAGE} />}
+            />
+            <FeatureBlock
+              copyKey="retain_control"
+              image={<img src={TEMPIMAGE} />}
+            />
+            <FeatureBlock
+              copyKey="push_boundaries"
+              image={<img src={TEMPIMAGE} />}
+            />
+            <FeatureBlock
+              copyKey="remonetize_endlessly"
+              image={<img src={TEMPIMAGE} />}
+            />
+          </div>
         </div>
       </div>
     );
