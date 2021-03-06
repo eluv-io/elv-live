deb738078df698536f592be7bf961e4754f5481a
Kevin Talmadge <kevin@eluv.io>
2021-03-05 18:12:49 -0800

    Update configuration to point to new site

--- src/EluvioConfiguration.js
+++ src/EluvioConfiguration.js
@@ -1,19 +1,8 @@
 const EluvioConfiguration = {
-  // Production test.net955203
   //"config-url": "https://main.net955305.contentfabric.io/config",
-
   "config-url": "https://demov3.net955210.contentfabric.io/config",
-
-  // Eluvio Live Site Object - PROD (https://live.eluv.io/) ro3 object: ilib2bLJtRJG3LW9yScSNStbjkSqF2hH  iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv
-  // "object-id": "iq__4PrRspxi3n5t3diS9PuCJMCAY6Rv",
-  // Eluvio Live Site Object - TEST/STAGING (https://live-stg-eluv-io.web.app/) DEV ro3 object: ilib2bLJtRJG3LW9yScSNStbjkSqF2hH  iq__23edex4Fwgt98PzR7mMrMHp8dw6E
-
-  "main-site-id": "iq__VLRP7NGGz2gwg9YEhMhhx69StP5",
-
-  // Test network
-  //"config-url": "https://test.net955203.contentfabric.io/config",
-  //"object-id": "iq__zK6CLwYBLhZmosiMXMe9eg2zErE",
-
+  //"main-site-id": "iq__VLRP7NGGz2gwg9YEhMhhx69StP5",
+  "main-site-id": "iq__2gkNh8CCZqFFnoRpEUmz7P3PaBQG",
   "bitmovin-license-key": "532a4784-591a-4039-8497-5feb80e5dd66"
 };
 
