fe39b5e2a7aa7444fafef627d649998aae52e9cb
Kevin Talmadge <kevin@eluv.io>
2021-03-05 16:34:50 -0800

    Technology page and placeholders for remaining pages

--- src/pages/main/Technology.js
+++ src/pages/main/Technology.js
@@ -1,7 +1,9 @@
-// Boilerplate for new class
-
 import React from "react";
 import {inject, observer} from "mobx-react";
+import Image from "Assets/icons/computer-diagrams.svg";
+
+import Copy from "./copy/Copy.yaml";
+import ImageIcon from "Common/ImageIcon";
 
 @inject("siteStore")
 @observer
@@ -9,7 +11,13 @@ class Technology extends React.Component {
   render() {
     return (
       <div className="page-content technology-page">
-        Tech
+        <div className="technology-page__image-container">
+          <ImageIcon className="technology-page__image" icon={Image} title="Eluv.io Technology" />
+        </div>
+        <div className="technology-page__copy-block">
+          <h2 className="technology-page__copy-header">Eluv.io Technology</h2>
+          <pre className="technology-page__copy">{ Copy.technology.text }</pre>
+        </div>
       </div>
     );
   }
--- src/pages/main/Terms.js
+++ src/pages/main/Terms.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {inject, observer} from "mobx-react";
 
-@inject("siteStore")
+@inject("mainStore")
 @observer
 class Terms extends React.Component {
   render() {
     return (
       <div className="page-content terms-page">
-        Terms
+        <h1 className="terms-page__header">Give us all your money</h1>
       </div>
     );
   }
