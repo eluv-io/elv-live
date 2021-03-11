4efb761f9b6aa169d7339dc0a2559a7ed1fbbae0
Kevin Talmadge <kevin@eluv.io>
2021-03-11 14:12:37 -0800

    Add promo video ; Change scroll behavior ; Make header fixed

--- src/pages/main/components/FeatureBlock.js
+++ src/pages/main/components/FeatureBlock.js
@@ -5,6 +5,8 @@ import UrlJoin from "url-join";
 import Copy from "../copy/Copy.yaml";
 import CardModal from "Pages/main/components/CardModal";
 import Modal from "Common/Modal";
+import BitmovinPlayer from "Common/BitmovinPlayer";
+import {DownArrow, UpArrow} from "Pages/main/components/NavigationArrows";
 
 @inject("mainStore")
 @observer
@@ -30,6 +32,11 @@ class FeatureBlock extends React.Component {
 
     const imageUrl = this.props.mainStore.MainSiteUrl(UrlJoin("images", this.props.copyKey, "main_image"));
 
+    let media = <img src={imageUrl} alt={copy.header} className="feature-block__image" />;
+    if(this.props.promoVideo && this.props.mainStore.promoPlayoutOptions[0]) {
+      media = <BitmovinPlayer playoutOptions={this.props.mainStore.promoPlayoutOptions[0]} muted scrollPlayPause />;
+    }
+
     return (
       <>
         {
@@ -41,7 +48,7 @@ class FeatureBlock extends React.Component {
         }
         <div className={`feature-block feature-block-${this.props.copyKey} scroll-block`} id={`scroll-block-${this.props.copyKey}`}>
           <div className="feature-block__image-container">
-            <img src={imageUrl} alt={copy.header} className="feature-block__image" />
+            { media }
           </div>
           <div className="feature-block__text-container">
             <h2 className="feature-block__header">{ copy.header }</h2>
@@ -56,6 +63,9 @@ class FeatureBlock extends React.Component {
               Learn More
             </button>
           </div>
+
+          <UpArrow />
+          <DownArrow />
         </div>
       </>
     );
