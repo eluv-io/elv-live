033d00d834435f483530a6105ff75533f0133903
Kevin Talmadge <kevin@eluv.io>
2021-03-15 22:04:33 -0700

    Fix page scaling issues with card modal ; Fix page loader and scroll to top

--- src/components/common/ScrollToTop.js
+++ src/components/common/ScrollToTop.js
@@ -2,9 +2,9 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 class ScrollToTop extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0)
+  async componentDidUpdate(prevProps) {
+    if(this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
     }
   }
 
