1ca9d5a5232cc2d3cea2dfe088a888e5e4fb49d0
Alec Jo <alecjo@Alecs-MacBook-Pro.local>
2021-03-15 20:24:12 -0700

    inital bug fixes

--- src/components/common/ScrollToTop.js
+++ src/components/common/ScrollToTop.js
@@ -2,19 +2,9 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 class ScrollToTop extends React.Component {
-  Scroll() {
-    document.body.scrollIntoView({behavior: "smooth"});
-    document.body.scrollTo({top: 0});
-    window.scrollTo({top: 0});
-  }
-
-  componentDidMount() {
-    this.Scroll();
-  }
-
-  async componentDidUpdate(prevProps) {
-    if(this.props.location.pathname !== prevProps.location.pathname) {
-      this.Scroll();
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0)
     }
   }
 
