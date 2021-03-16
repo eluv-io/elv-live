103215330e2db9f1f56211b83f0603cb44afcab2
Kevin Talmadge <kevin@eluv.io>
2021-03-15 22:47:38 -0700

    Add terms and privacy policy ; Fix initialization of EluvioPlayer

--- src/components/common/ScrollToTop.js
+++ src/components/common/ScrollToTop.js
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom";
 class ScrollToTop extends React.Component {
   async componentDidUpdate(prevProps) {
     if(this.props.location !== prevProps.location) {
+      await new Promise(resolve => setTimeout(resolve, 100));
       window.scrollTo(0, 0);
     }
   }
