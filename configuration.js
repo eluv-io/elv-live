af25a248db76aac8b04fce0148e1e5f91cb1c71c
Kevin Talmadge <kevin@eluv.io>
2021-03-24 16:39:24 -0700

    Move configuration

---
+++ configuration.js
@@ -0,0 +1,10 @@
+// Overwritten by deploy script
+
+const EluvioConfiguration = {
+  "config-url": "https://demov3.net955210.contentfabric.io/config",
+  "live-site-id": "iq__3CXH4CDJxX76wCV6YkfEqVmSxmHp",
+  "version": "elv-live@main",
+  "version-hash": "elv-live@cac61fc90f7183eee06d3996bb65b9e71a8b6f06"
+};
+
+export default EluvioConfiguration;
