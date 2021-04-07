b65e811b2c6a668fcc129dea6259ae9e394e84c8
Kevin Talmadge <kevin@eluv.io>
2021-04-07 12:37:17 -0700

    Add coinbase checkout

--- src/pages/event/PromoPlayer.js
+++ src/pages/event/PromoPlayer.js
@@ -1,7 +1,5 @@
 import React from "react";
 import {inject, observer} from "mobx-react";
-import UrlJoin from "url-join";
-
 import EluvioPlayer, {EluvioPlayerParameters} from "@eluvio/elv-player-js";
 
 import EluvioConfiguration from "EluvioConfiguration";
