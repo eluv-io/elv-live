ed2cc394018854f792283e0580f57aaeed6d1e51
Kevin Talmadge <kevin@eluv.io>
2021-04-29 14:20:02 -0700

    Fix tag manager initialization

--- src/stores/Site.js
+++ src/stores/Site.js
@@ -366,14 +366,11 @@ class SiteStore {
       }
 
       if(analytics.google_tag_manager_id) {
-        ((w,d,s,l,i) => {
-          w[l]=w[l]||[];
-          w[l].push({"gtm.start": new Date().getTime(),event:"gtm.js"});
-          var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!=="dataLayer"?"&l="+l:"";
-          j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-        })(window, document, "script", "dataLayer", analytics.google_tag_manager_id);
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer',analytics.google_tag_manager_id);
       }
     } catch(error) {
       console.error("Failed to load Google Analytics:");
