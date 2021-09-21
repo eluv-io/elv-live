121aa4cf1eb141d39d4b9829d74f6f3e19dec2b3
kevin.talmadge@eluv.io <kevin@eluv.io>
2021-09-20 18:24:48 -0700

    Update wallet client ; Pass event ID to embedded wallet

--- src/pages/wallet/WalletFrame.js
+++ src/pages/wallet/WalletFrame.js
@@ -2,14 +2,21 @@ import React from "react";
 import {inject, observer} from "mobx-react";
 
 @inject("rootStore")
+@inject("siteStore")
 @observer
 class WalletFrame extends React.Component {
   render() {
+    if(!this.props.siteStore.currentSiteInfo || !this.props.siteStore.siteId) { return null; }
+
     return (
       <div className="wallet-panel" id="wallet-panel">
         <div
           className="wallet-target"
-          ref={element => this.props.rootStore.InitializeWalletClient(element)}
+          ref={element => this.props.rootStore.InitializeWalletClient({
+            target: element,
+            eventId: this.props.siteStore.siteId,
+            darkMode: this.props.siteStore.darkMode
+          })}
         >
         </div>
       </div>
