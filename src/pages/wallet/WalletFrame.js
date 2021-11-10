924b2b61530adbe2b1061a9e032e4375cc6169a5
kevin.talmadge@eluv.io <kevin@eluv.io>
2021-11-09 19:27:18 -0800

    Don't show popup login modal on drop event if login is not required

--- src/pages/wallet/WalletFrame.js
+++ src/pages/wallet/WalletFrame.js
@@ -17,6 +17,7 @@ class WalletFrame extends React.Component {
         {
           !this.props.rootStore.walletLoggedIn &&
           this.props.rootStore.currentWalletState.visibility !== "hidden" &&
+          this.props.rootStore.currentWalletState.requireLogin &&
           !window.location.pathname.startsWith("/wallet")
             ?
             <LoginModal />
