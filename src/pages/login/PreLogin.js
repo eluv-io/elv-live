829c50141bf8c8848ec268ec7a405a101ef55fcc
kevin.talmadge@eluv.io <kevin@eluv.io>
2022-02-02 11:10:34 -0800

    Initial changes for pre-login

--- src/pages/login/PreLogin.js
+++ src/pages/login/PreLogin.js
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
+import {render} from "react-dom";
+import Modal from "Common/Modal";
+import ReactMarkdown from "react-markdown";
+import SanitizeHTML from "sanitize-html";
 
-const PreLogin = ({onComplete}) => {
+const PreLoginMS = ({onComplete}) => {
   const [country, setCountry] = useState("United States");
 
   return (
@@ -38,4 +42,110 @@ const PreLogin = ({onComplete}) => {
   );
 };
 
-export default PreLogin;
+const PrivacyPolicy = () => {
+  const [policyModal, setPolicyModal] = useState(null);
+
+  const policy = {
+    rich_text: "<h1>Rich Text</h1>",
+    html: undefined
+  };
+
+  let link;
+  if(policy.url) {
+    link = (
+      <a
+        target="_blank"
+        className="pre-login__privacy-link"
+        rel="noopener"
+        href={policy.url}
+      >
+        Privacy Policy
+      </a>
+    );
+  } else if(policy.rich_text || policy.html) {
+    link = (
+      <button
+        className="pre-login__privacy-link"
+        onClick={() => {
+          setPolicyModal(
+            <Modal
+              className="event-message-container"
+              Toggle={() => setPolicyModal(null)}
+            >
+              {
+                policy.rich_text ?
+                  <div className="event-message">
+                    <div className="event-message__content">
+                      <div
+                        className="event-message__content__message"
+                        ref={element => {
+                          if(!element) {
+                            return;
+                          }
+
+                          render(
+                            <ReactMarkdown linkTarget="_blank" allowDangerousHtml>
+                              {SanitizeHTML(policy.rich_text)}
+                            </ReactMarkdown>,
+                            element
+                          );
+                        }}
+                      />
+                    </div>
+                  </div> :
+                  <iframe
+                    className="event-message"
+                    src={policy.html.url}
+                  />
+              }
+            </Modal>
+          );
+        }}
+      >
+        Privacy Policy
+      </button>
+    );
+  } else {
+    return null;
+  }
+
+  return (
+    <>
+      { policyModal }
+      { link }
+    </>
+  );
+};
+
+const PreLogin = ({onComplete}) => {
+  const [consent, setConsent] = useState(true);
+
+  return (
+    <div className="pre-login">
+      <div className="pre-login__text">
+        <h2 className="pre-login__header">
+          This is a consent form with custom text
+        </h2>
+
+        <div className="pre-login__consent">
+          <input name="consent" type="checkbox" checked={consent} onChange={() => setConsent(!consent)} className="pre-login__consent-checkbox" />
+          <label htmlFor="consent" className="pre-login__consent-label"  onClick={() => setConsent(!consent)}>
+            By checking this box, I agree to share my email with Eluvio and its partners
+          </label>
+        </div>
+      </div>
+
+      <div className="pre-login__actions">
+        <button className="login-page__login-button login-page__login-button-pre-login pre-login__button" onClick={() => onComplete({consent})}>
+          Continue
+        </button>
+      </div>
+
+      <div className="pre-login__privacy-policy">
+        <PrivacyPolicy />
+      </div>
+    </div>
+  );
+};
+
+export default PreLoginMS;
