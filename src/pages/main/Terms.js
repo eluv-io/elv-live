a5d8e3d83be8d2391e40247cf3a19060d8ad6e43
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-01-16 16:33:41 -0800

    Update terms

--- src/pages/main/Terms.js
+++ src/pages/main/Terms.js
@@ -28,7 +28,7 @@ class Terms extends React.Component {
   render() {
     return (
       <div className="page-content terms-page">
-        <h1 className="terms-page__header">Terms</h1>
+        <h1 className="terms-page__header">Eluvio End User Agreement</h1>
         <iframe className="terms-page__document-frame" src={this.state.termsUrl} />
       </div>
     );
