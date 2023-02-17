555a03aa72b3e85e4ba3f25605955da4954dad8f
elv-zenia <zenia.villa@eluv.io>
2023-02-17 15:39:55 -0800

    Add mobile nav

--- src/main/components/Header.js
+++ src/main/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {observer} from "mobx-react";
 import ImageIcon from "./ImageIcon";
@@ -8,7 +8,7 @@ import {useLocation} from "react-router";
 
 import EluvioLogo from "../static/images/logos/eluvio-logo-color.png";
 
-import {MenuIcon, XIcon} from "../static/icons/Icons";
+import {DiscoverIcon, MenuIcon, ProfileIcon, SocialIcons, WalletIcon, XIcon} from "../static/icons/Icons";
 import {RichText} from "./Misc";
 
 const NotificationBanner = observer(({className=""}) => {
@@ -31,6 +31,7 @@ const NotificationBanner = observer(({className=""}) => {
 const Header = observer(() => {
   const notificationBanner = <NotificationBanner className={uiStore.pageWidth > 1000 ? "desktop" : "mobile"} />;
   const location = useLocation();
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   return (
     <>
@@ -94,8 +95,48 @@ const Header = observer(() => {
             Discover Projects
           </Action>
         </nav>
-        <Action icon={MenuIcon} className="dark header__mobile-nav-button mobile" onClick={() => {}} />
+        <Action icon={MenuIcon} className="dark header__mobile-nav-button mobile" onClick={() => setShowMobileMenu(prevState => !prevState)} />
       </header>
+
+      <div className={`mobile-nav__menu ${showMobileMenu ? "" : "hidden"}`}>
+        <Action icon={XIcon} className="dark mobile-nav__close-button mobile" onClick={() => setShowMobileMenu(false)} />
+        <div className="mobile-nav__menu-content">
+          <div className="mobile-nav__menu-header mobile-nav__menu-section">
+            <Action className="mobile-nav__menu-header-item" icon={DiscoverIcon} alt="Projects" to="/about">
+              <span>Projects</span>
+            </Action>
+            <Action className="mobile-nav__menu-header-item" icon={ProfileIcon} alt="Profile" to="/">
+              <span>Profile</span>
+            </Action>
+            <Action className="mobile-nav__menu-header-item" icon={WalletIcon} alt="Wallet" to="/">
+              <span>Wallet</span>
+            </Action>
+          </div>
+          <div className="mobile-nav__menu-links mobile-nav__menu-section mobile-nav__menu-primary-links">
+            <Action to="/creators-and-publishers">Creators & Publishers</Action>
+            <Action to="/content-fabric">Content Fabric</Action>
+            <Action to="/content-fabric/technology">Technology</Action>
+            <Action to="/content-fabric/blockchain">Blockchain</Action>
+            <Action to="/features/details">Features</Action>
+          </div>
+          <hr className="mobile-nav__menu-section-line" />
+
+          <div className="mobile-nav__menu-links mobile-nav__menu-section mobile-nav__menu-secondary-links">
+            <Action className="link" to="/features/pricing">Pricing</Action>
+            <Action className="link" to="/features/support">Support</Action>
+            <Action className="link" to="/community">Community</Action>
+            <Action className="link" to="/about/partners">Partners</Action>
+            <Action className="link" to="/about/news">News</Action>
+            <Action className="link" to="/about/contact">Contact Us</Action>
+            <div className="mobile-nav__menu-social-links">
+              <Action className="dark" to="https://www.instagram.com/eluvioinc" icon={SocialIcons.InstagramIcon} alt="Instagram" />
+              <Action className="dark" to="https://twitter.com/eluvioinc" icon={SocialIcons.TwitterIcon} alt="Twitter" />
+              <Action className="dark" to="https://www.facebook.com/EluvioInc" icon={SocialIcons.FacebookIcon} alt="Facebook" />
+              <Action className="dark" to="https://www.linkedin.com/company/eluv-io" icon={SocialIcons.LinkedInIcon} alt="LinkedIn" />
+            </div>
+          </div>
+        </div>
+      </div>
 
       { uiStore.pageWidth > 1000 ? notificationBanner : null }
     </>
