c4494a47688d796aed0e66fa8c8c35a6cdf5beb2
elv-zenia <zenia.villa@eluv.io>
2023-04-10 18:57:43 -0700

    Verification bugs

--- src/main/pages/technology/LearnMore.js
+++ src/main/pages/technology/LearnMore.js
@@ -7,26 +7,39 @@ import {DocumentIcon, LinkIcon, TechnologyIcons} from "../../static/icons/Icons"
 
 import Whitepaper from "./documents/EluvioContentFabricProtocolWhitepaper.pdf";
 
-const LearnMore = observer(() => {
+const LearnMore = observer(({contentFabric=false}) => {
+  const contentFabricLink = [{
+    to: "/content-fabric",
+    target: "_blank",
+    text: mainStore.l10n.actions.content_fabric_section,
+    icon: LinkIcon
+  }];
+
+  const technologyLink = [{
+    to: "/content-fabric/technology",
+    target: "_blank",
+    text: mainStore.l10n.actions.technology_section,
+    icon: LinkIcon
+  }];
+
+  const whitepaperLink = [{
+    to: Whitepaper,
+    target: "_blank",
+    text: mainStore.l10n.actions.whitepaper,
+    icon: DocumentIcon
+  }];
+
+  const links = [
+    ...(contentFabric ? contentFabricLink : technologyLink),
+    ...whitepaperLink
+  ];
+
   return (
     <InfoBox
       icon={TechnologyIcons.LearnMoreTechnologyIcon}
-      header={mainStore.l10n.content_fabric.learn_more.header}
-      content={mainStore.l10n.content_fabric.learn_more.text}
-      links={[
-        {
-          to: "/content-fabric/technology",
-          target: "_blank",
-          text: mainStore.l10n.actions.technology_section,
-          icon: LinkIcon
-        },
-        {
-          to: Whitepaper,
-          target: "_blank",
-          text: mainStore.l10n.actions.whitepaper,
-          icon: DocumentIcon
-        }
-      ]}
+      header={contentFabric ? mainStore.l10n.content_fabric.learn_more.content_fabric_header : mainStore.l10n.content_fabric.learn_more.header}
+      content={contentFabric ? mainStore.l10n.content_fabric.learn_more.content_fabric_text : mainStore.l10n.content_fabric.learn_more.text}
+      links={links}
     />
   );
 });
