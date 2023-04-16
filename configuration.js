79dddefb6e19d2a00fceb3312ecaa39563b194fd
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-04-16 11:09:35 -0700

    Fix notification wrapping issue

--- configuration.js
+++ configuration.js
@@ -4,9 +4,9 @@ const EluvioConfiguration = {
   "network": "demo",
   "mode": "staging",
 
-  //"network": "main",
-  //"mode": "staging",
-  //"mode": "production",
+  "network": "main",
+  "mode": "staging",
+  "mode": "production",
 
   "version": "elv-live@main",
   "version-hash": "elv-live@cac61fc90f7183eee06d3996bb65b9e71a8b6f06"
