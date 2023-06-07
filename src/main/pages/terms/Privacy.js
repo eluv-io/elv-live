924c3c8bae74eea5325fbbba5cd2343958588edf
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-06-06 22:03:18 -0700

    Remove headers from terms pages

--- src/main/pages/terms/Privacy.js
+++ src/main/pages/terms/Privacy.js
@@ -1,21 +1,16 @@
 import React from "react";
 import PrivacyDocument from "../../static/documents/PrivacyPolicy.html";
-import {mainStore} from "../../stores/Main";
-import {observer} from "mobx-react";
 
-const Privacy = observer(() => {
+const Privacy = () => {
   const documentUrl = window.URL.createObjectURL(new Blob([PrivacyDocument], {type: "text/html"}));
 
   return (
     <div className="page light">
-      <div className="page__header-container">
-        <h1>{ mainStore.l10n.documents.privacy.header }</h1>
-      </div>
       <div className="terms">
         <iframe src={documentUrl} className="terms__frame" />
       </div>
     </div>
   );
-});
+};
 
 export default Privacy;
