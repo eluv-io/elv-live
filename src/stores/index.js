d3e57fc269668a348f294a063cdc8818338593d9
kevin.talmadge@eluv.io <kevin@eluv.io>
2023-11-29 12:01:54 -0800

    Add hide option to header links ; Add redirect to owned options to banners

--- src/stores/index.js
+++ src/stores/index.js
@@ -15,7 +15,7 @@ configure({
 });
 
 let walletAppUrl;
-if(window.location.hostname.startsWith("192.") || window.location.hostname.startsWith("elv-test.io")) {
+if(window.location.hostname === "localhost" || window.location.hostname.startsWith("192.") || window.location.hostname.startsWith("elv-test.io")) {
   walletAppUrl = `https://${window.location.hostname}:8090`;
 } else if(window.location.hostname.startsWith("live-stg")) {
   walletAppUrl = EluvioConfiguration.network === "main" ?
