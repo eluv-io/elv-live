c7a072302cbcf6f3dbb664ce65af2f3b834aa156
Zenia Villa <91760982+elv-zenia@users.noreply.github.com>
2023-11-28 16:15:24 -0800

    Update Streaming and Reporting doc

--- src/main/pages/features/Features.js
+++ src/main/pages/features/Features.js
@@ -8,7 +8,7 @@ import {observer} from "mobx-react";
 import {DocumentIcon, NodeGroupIcon, PlayCircleIcon, TechnologyIcons, Web3Icon} from "../../static/icons/Icons";
 import SupportGrid from "./SupportGrid";
 
-import StreamingAndReportingDoc from "../technology/documents/StreamingAndReporting-2023-09.pdf";
+import StreamingAndReportingDoc from "../technology/documents/StreamingAndReporting-2023-11.pdf";
 
 const ItemCard = (data, dark=false) => {
   const {contentTitle, description, link} = data;
