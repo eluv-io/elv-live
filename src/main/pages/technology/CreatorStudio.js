0e141a993cdffdd8bb2dc08dde176ad74f3e9a60
elv-zenia <zenia.villa@eluv.io>
2024-04-05 18:02:09 -0700

    Add creator studio header

--- src/main/pages/technology/CreatorStudio.js
+++ src/main/pages/technology/CreatorStudio.js
@@ -1,12 +1,20 @@
 import React from "react";
+import {observer} from "mobx-react";
+import {mainStore} from "../../stores/Main";
 
-const CreatorStudio = () => {
+const CreatorStudio = observer(() => {
+  const copy = mainStore.l10n.casablanca;
+
   return (
     <div className="page light">
+      <div className="page__header-container">
+        <h1>{copy.title}</h1>
+        <h3>{copy.header}</h3>
+      </div>
       <div className="page__content-block"></div>
     </div>
   );
-};
+});
 
 export default CreatorStudio;
 
