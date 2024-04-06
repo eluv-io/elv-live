25a0a2996933db7027480db54a78bdcd0bbe3cf5
elv-zenia <zenia.villa@eluv.io>
2024-04-05 18:41:19 -0700

    Add creator studio sections

---
+++ src/main/pages/technology/AppSuiteControlPanel.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {observer} from "mobx-react";
+import {mainStore} from "../../stores/Main";
+import {ApplicationIcons} from "../../static/icons/Icons";
+import {Action} from "../../components/Actions";
+import ImageIcon from "../../components/ImageIcon";
+
+const AppSuiteControlPanel = observer(() => {
+  const copy = mainStore.l10n.casablanca;
+
+  const APP_ICON_MAP = {
+    "creator_studio": ApplicationIcons.CreatorStudioIcon,
+    "analytics": ApplicationIcons.ContentAnalyticsIcon,
+    "clip_search": ApplicationIcons.AiClipSearchIcon,
+    "video_editor": ApplicationIcons.VideoEditorIcon,
+    "media_wallet": ApplicationIcons.MediaWalletIcon,
+    "embeddable_player": ApplicationIcons.EmbeddablePlayerIcon,
+    "tag_new": ApplicationIcons.NewTagIcon,
+    "tag_v2": ApplicationIcons.V2TagIcon
+  };
+
+  return (
+    <div className="page__content-block">
+      <div className="curved-box application-suite-info light">
+        <h3 className="application-suite-info__header">
+          {copy.app_suite_title}
+        </h3>
+        <div className="application-suite-info__icons">
+          {
+            copy.application_suite.map(app => (
+              <Action to={app.to} target="_blank" key={`app-suite-${app.title}`}>
+                <div className="application-suite-info__icon">
+                  <ImageIcon icon={APP_ICON_MAP[app.icon]} className="application-suite-info__icon__icon" />
+                  <div className="application-suite-info__icon__text">
+                    { app.title }
+                  </div>
+                  <ImageIcon icon={APP_ICON_MAP[app.tag]} className="application-suite-info__icon__tag" />
+                </div>
+              </Action>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default AppSuiteControlPanel;
--- src/main/pages/technology/CreatorStudio.js
+++ src/main/pages/technology/CreatorStudio.js
@@ -1,17 +1,76 @@
 import React from "react";
 import {observer} from "mobx-react";
 import {mainStore} from "../../stores/Main";
+import {Accordion, AccordionGroup, InfoBox, RichText} from "../../components/Misc";
+import ImageIcon from "../../components/ImageIcon";
+import {ApplicationIcons, DocumentIcon, TechnologyIcons} from "../../static/icons/Icons";
+import AppSuiteControlPanel from "./AppSuiteControlPanel";
 
 const CreatorStudio = observer(() => {
-  const copy = mainStore.l10n.casablanca;
+  const copy = mainStore.l10n.casablanca.pages.creator_studio;
 
   return (
     <div className="page light">
       <div className="page__header-container">
-        <h1>{copy.title}</h1>
-        <h3>{copy.header}</h3>
+        <h1>{mainStore.l10n.casablanca.title}</h1>
+        <h3>{mainStore.l10n.casablanca.header}</h3>
       </div>
-      <div className="page__content-block"></div>
+      <div className="page__content-block">
+        <div className="application-info__header">
+          <div className="application-info__header-title">{copy.header}</div>
+          <div className="application-info__title-group">
+            <ImageIcon icon={ApplicationIcons.CreatorStudioIcon} className="application-info__icon"/>
+            <div className="application-info__header-text light">{copy.title}</div>
+            <ImageIcon icon={ApplicationIcons.NewTagIcon} className="application-info__tag-icon"/>
+          </div>
+          <RichText richText={copy.header_description} className="application-info__short-description page__copy fade-in--slow"/>
+          <RichText richText={copy.full_description} className="application-info__full-description page__copy fade-in--slow"/>
+        </div>
+      </div>
+      <AppSuiteControlPanel/>
+      {
+        copy.accordion_sections.map(section => (
+          <div className="page__content-block" key={`creator-studio-section-${section.header}`}>
+            <AccordionGroup
+              key={`creator-studio-accordion-section-${section.header}`}
+              header={section.items ? section.header : ""}
+            >
+              {
+                section.items ?
+                  section.items.map(item => (
+                    <Accordion title={item.title} key={`creator-studio-accordion-item-${item.title}`}>
+                      <RichText className="accordion__description-card" richText={item.description}/>
+                    </Accordion>
+                  )) :
+                  <Accordion title={section.header} hasHeader={false}>
+                    <RichText className="accordion__description-card" richText={section.description}/>
+                  </Accordion>
+              }
+            </AccordionGroup>
+          </div>
+        ))
+      }
+      <div className="page__content-block">
+        <InfoBox
+          icon={TechnologyIcons.FabricBrowserIcon}
+          header={copy.info_link.title}
+          content={copy.info_link.description}
+          links={[
+            {
+              // href: ,
+              text: copy.info_link.links[0].text,
+              icon: DocumentIcon,
+              target: "_blank"
+            },
+            {
+              // href: ,
+              text: copy.info_link.links[1].text,
+              icon: DocumentIcon,
+              target: "_blank"
+            }
+          ]}
+        />
+      </div>
     </div>
   );
 });
