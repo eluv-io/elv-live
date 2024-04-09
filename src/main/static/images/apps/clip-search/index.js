0e3b3f885aa55f5394102d72b67cbd710fc27ca7
elv-zenia <zenia.villa@eluv.io>
2024-04-08 17:09:35 -0700

    Update clip search images

--- src/main/static/images/apps/clip-search/index.js
+++ src/main/static/images/apps/clip-search/index.js
@@ -1,2 +1,8 @@
 export const ClipSearch1 = require("./01.jpg");
 export const ClipSearch2 = require("./02.jpg");
+export const ClipSearch3 = require("./03.jpg");
+export const ClipSearch4 = require("./04.jpg");
+export const ClipSearch5 = require("./05.jpg");
+export const ClipSearch6 = require("./06.jpg");
+export const ClipSearch7 = require("./07.jpg");
+export const ClipSearch8 = require("./08.jpg");
