c97c9ba4f7b4a5c84c63c03502264945c4dd1f5b
elv-zenia <zenia.villa@eluv.io>
2024-10-09 12:38:41 -0700

    Remove unused import

--- src/main/pages/register/Register.js
+++ src/main/pages/register/Register.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {observer} from "mobx-react";
 import {uiStore, mainStore} from "../../stores/Main";
 
