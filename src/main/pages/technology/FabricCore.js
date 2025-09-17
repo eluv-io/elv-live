3df595b71bc4e90f6ef97232493d7297bf9aebd2
Zenia Villa <zeniavilla@hotmail.com>
2025-09-17 10:14:40 -0700

    Update features accordion

--- src/main/pages/technology/FabricCore.js
+++ src/main/pages/technology/FabricCore.js
@@ -18,17 +18,38 @@ const FabricCore = observer(() => {
       </div>
       <Video versionHash={copy.header_video} className="main-page-block__video main-page-block__core-video" />
       <div className="page__content-block">
-        <div key={`fabric-core-section-${copy.summary_section.title}`} className="page__content-block">
+        <div className="page__content-block">
           <h3 className="page__content-block__header light">{copy.summary_section.title}</h3>
           <RichText richText={copy.summary_section.description} className="page__copy fade-in--slow"/>
         </div>
+      </div>
+
+      <div className="page__content-block">
+        {
+          copy.upper_accordion_sections.map(section => (
+            <div key={`fabric-core-accordion-section-${section.title}`} className="page__content-block">
+              <AccordionGroup header={section.items ? section.header : ""}>
+                {
+                  section.items ?
+                    section.items.map(item => (
+                      <Accordion title={item.title} defaultOpen key={`fabric-core-accordion-item-${item.title}`} id={item.id}>
+                        <RichText className="accordion__description-card" richText={item.description}/>
+                      </Accordion>
+                    )) :
+                    <Accordion title={section.header} hasHeader={false} defaultOpen id={section.id}>
+                      <RichText className="accordion__description-card" richText={section.description}/>
+                    </Accordion>
+                }
+              </AccordionGroup>
+            </div>
+          ))
+        }
       </div>
       <AppSuiteControlPanel />
       {
         copy.accordion_sections.map(section => (
           <div className="page__content-block" key={`fabric-core-section-${section.header}`}>
             <AccordionGroup
-              key={`fabric-core-accordion-section-${section.header}`}
               header={section.items ? section.header : ""}
             >
               {
